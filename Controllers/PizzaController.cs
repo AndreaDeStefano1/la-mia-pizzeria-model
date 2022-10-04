dcca0398f5e15f9e86b579632e317ea5d0f488b3
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-10-04 14:31:13 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,36 @@
+﻿using la_mia_pizzeria_model.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_model.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Pizza> pizzaList = new List<Pizza>();
+            pizzaList.Add(new Pizza() { Name = "Margherita", Description = "Pomodoro, mozzarella, basilico", Image = "/img/pizza.jpg", Price = 4.5 });
+            pizzaList.Add(new Pizza() { Name = "Divola", Description = "Pomodoro, salame, mozzarella", Image = "/img/pizza.jpg", Price = 5 });
+            pizzaList.Add(new Pizza() { Name = "marinara", Description = "Pomodoro, mozzarella, basilico", Image = "/img/pizza.jpg", Price = 3.5 });
+            return View(pizzaList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace la_mia_pizzeria_model.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+    }
+}
