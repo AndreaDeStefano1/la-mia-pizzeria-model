644b6f9e4c6a9e9e3a4a7bec748a305f49dcd278
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-10-04 15:38:16 +0200

    Fine

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -16,12 +16,20 @@ namespace la_mia_pizzeria_model.Controllers
         public IActionResult Index()
         {
             List<Pizza> pizzaList = new List<Pizza>();
-            pizzaList.Add(new Pizza() { Name = "Margherita", Description = "Pomodoro, mozzarella, basilico", Image = "/img/pizza.jpg", Price = 4.5 });
-            pizzaList.Add(new Pizza() { Name = "Divola", Description = "Pomodoro, salame, mozzarella", Image = "/img/pizza.jpg", Price = 5 });
-            pizzaList.Add(new Pizza() { Name = "marinara", Description = "Pomodoro, mozzarella, basilico", Image = "/img/pizza.jpg", Price = 3.5 });
+            pizzaList.Add(new Pizza() { Id = 1, Name = "Margherita", Description = "Pomodoro, mozzarella, basilico", Image = "/img/pizza.jpg", Price = 4.5 });
+            pizzaList.Add(new Pizza() { Id = 2, Name = "Divola", Description = "Pomodoro, salame, mozzarella", Image = "/img/pizza.jpg", Price = 5 });
+            pizzaList.Add(new Pizza() { Id = 3, Name = "marinara", Description = "Pomodoro, mozzarella, basilico", Image = "/img/pizza.jpg", Price = 3.5 });
             return View(pizzaList);
         }
-
+        public IActionResult Show(int id)
+        {
+            List<Pizza> pizzaList = new List<Pizza>();
+            pizzaList.Add(new Pizza() { Id = 1, Name = "Margherita", Description = "Pomodoro, mozzarella, basilico", Image = "/img/pizza.jpg", Price = 4.5 });
+            pizzaList.Add(new Pizza() { Id = 2, Name = "Divola", Description = "Pomodoro, salame, mozzarella", Image = "/img/pizza.jpg", Price = 5 });
+            pizzaList.Add(new Pizza() { Id = 3, Name = "marinara", Description = "Pomodoro, mozzarella, basilico", Image = "/img/pizza.jpg", Price = 3.5 });
+            Pizza pizza = pizzaList[id];
+            return View(pizza);
+        }
         public IActionResult Privacy()
         {
             return View();
